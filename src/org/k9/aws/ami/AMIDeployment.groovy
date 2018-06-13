1adc9f36a7a43e9a357678b24c2d4564f08cda4c
sushmahebbar <sushma.m@kloud9.nyc>
2018-06-13 17:24:00 +0530

    Update Jenkinsfile

---
+++ src/org/k9/aws/ami/AMIDeployment.groovy
@@ -0,0 +1,27 @@
+package org.k9.aws.ami
+
+import groovy.time.*
+import java.text.*
+import org.k9.*
+import org.k9.aws.ec2.autoscale.AutoScaling
+
+class AMIDeployment implements Serializable {
+    def config
+    def script
+    def output
+
+    AMIDeployment(script, config) {
+        this.script = script
+        this.config = config
+        this.output = [:]
+    }
+
+    // def CreateLaunchconfig(){
+    // def httpObj = new http.SimpleHTTPBuilder(this.script,this.params.launchconfig)
+    // httpObj.sendRequest()
+    // }
+def deploy() {
+     def asg = new AutoScaling(this.script, this.config)
+     def elb = new LoadBalancer(this.script, this.config)
+     def createStatus = utils.createAWSResources(asg, elb)
+}
