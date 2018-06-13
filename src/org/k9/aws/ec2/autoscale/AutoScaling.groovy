e5d0be84619abdbb3ee8577a2a6284a2a8a7c225
sushmahebbar <sushma.m@kloud9.nyc>
2018-06-13 18:19:23 +0530

    update amideployment

--- src/org/k9/aws/ec2/autoscale/AutoScaling.groovy
+++ src/org/k9/aws/ec2/autoscale/AutoScaling.groovy
@@ -12,18 +12,12 @@ class AutoScaling implements Serializable {
         // this.output = output
     }
  
- def createLaunchConfig(def output) {
-        def funcOutput = [:]
+ def createLaunchConfig() {
         this.config.launchConf['jsonBody']['launchConfigurationName'] = "${this.config.deploymentName}-${this.config.subEnv}-lc-${output.dbData.version}-${output.dbData.buildNo}"
         this.config.launchConf['jsonBody']['userData'] = "https://s3-us-west-2.amazonaws.com/${output.global['userDataScript']}/scriptViaGroovy.sh"
         //this.config.launchConf['httpParams'] = this.script.awsVars.lcHttpParams
 
         def lcOut = new http.SimpleHTTPBuilder(this.script,this.config.launchConf).sendRequest()
-        if (lcOut['response'] == "error") {
-                funcOutput['response'] = "error"
-                return funcOutput
-        }
+      
 
-        funcOutput['response'] = "success"
-        funcOutput['global'] = ['launchConfName': this.config.launchConf['jsonBody']['launchConfigurationName'] ]
  }
